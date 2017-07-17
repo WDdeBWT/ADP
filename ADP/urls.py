bca6a466bb35db6dbbc9e1e81e74fa9d6d7b6f33
virlaser <liziyan784617756@foxmail.com>
2017-07-18 00:03:19 +0800

    添加了对应APP URL的配置

--- ADP/urls.py
+++ ADP/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+import xadmin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # xadmin
+    url(r'^xadmin/', xadmin.site.urls),
+    # ctf
+    url(r'^ctf/', include('ctf.urls', namespace="ctf")),
 ]
