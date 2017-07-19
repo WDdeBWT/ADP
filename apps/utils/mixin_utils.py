4e40eeac390eb8120318b812987d0c1253fe8494
baiwt <bwt1997@126.com>
2017-07-19 11:17:58 +0800

    add utils/minin_utilis

---
+++ apps/utils/mixin_utils.py
@@ -0,0 +1,12 @@
+# _*_ coding:utf-8 _*_
+__author__ = "WDdeBWT"
+__date__ = "2017/7/19 11:15"
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required(login_url='/login/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
