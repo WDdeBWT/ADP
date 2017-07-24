afc1f36e700ffe154bc94c6fd894d5b43e70df42
806025582 <806025582@qq.com>
2017-07-24 14:34:38 +0800

    功能增加

--- apps/experiments/views.py
+++ apps/experiments/views.py
@@ -17,32 +17,59 @@ class ExpView(View):
     """
 
     def  get(self,request):
+
+        CATEGORY_CHOICES = {
+            "sql": "SQL注入漏洞",
+            "xss": "跨站脚本漏洞",
+            "weak_password": "弱口令漏洞",
+            "http": "HTTP报头追踪漏洞",
+            "struct2": "Struct2远程命令执行漏洞",
+            "fishing": "框架钓鱼漏洞",
+            "file_upload": "文件上传漏洞",
+            "script": "应用程序测试脚本泄露",
+            "ip": "私有IP地址泄露漏洞",
+            "login": "未加密登录请求",
+            "message": "敏感信息泄露漏洞",
+        }
+        CATEGORY_CHOICES2 = {
+            "SQL注入漏洞":"sql",
+            "跨站脚本漏洞":"xss",
+            "弱口令漏洞":"weak_password",
+            "HTTP报头追踪漏洞":"http",
+            "Struct2远程命令执行漏洞":"struct2",
+            "框架钓鱼漏洞":"fishing",
+            "文件上传漏洞":"file_upload",
+            "应用程序测试脚本泄露":"script",
+            "私有IP地址泄露漏洞":"ip",
+            "未加密登录请求":"login",
+            "敏感信息泄露漏洞":"message",
+        }
+        DEGREE = {
+            "cj": "初级",
+            "zj": "中级",
+            "gj": "高级",
+        }
+
         #得到所有实验
-        all_exps = Experiment.objects.all()
+        all_exps = Experiment.objects.order_by("-click_nums")
         #按点击数取前四个热门实验
         hot_exps = all_exps.order_by("-click_nums")[:4]
 
-        CATEGORY_CHOICES = {
-            "sql":"SQL注入漏洞",
-            "xss":"跨站脚本漏洞",
-            "weak_password":"弱口令漏洞",
-            "http":"HTTP报头追踪漏洞",
-            "struct2":"Struct2远程命令执行漏洞",
-            "fishing":"框架钓鱼漏洞",
-            "file_upload":"文件上传漏洞",
-            "script":"应用程序测试脚本泄露",
-            "ip":"私有IP地址泄露漏洞",
-            "login":"未加密登录请求",
-            "message":"敏感信息泄露漏洞",
-        }
-        DEGREE={
-            "cj":"初级",
-            "zj":"中级",
-            "gj":"高级",
-        }
+        category = request.GET.get('category',"")
+        if category == '':
+            pass
+        else:
+            try:
+                category=CATEGORY_CHOICES2[category]
+            except:
+                category=''
+            all_exps = Experiment.objects.filter(category=category).order_by("-click_nums")
+
+        tags = []
         for hot_exp in hot_exps:
             hot_exp.category=CATEGORY_CHOICES[hot_exp.category]
             hot_exp.degree=DEGREE[hot_exp.degree]
+            tags.append(hot_exp.category)
 
         exp_comment_objects = UserComments.objects.filter(comment_type=2).order_by("-add_time")
         # 得到ctf课程,直接将ctf课程属性添加进ctf评论类,这样评论就可以显示来自什么题目
@@ -57,11 +84,11 @@ class ExpView(View):
                 exp_comment_object.delete()
         # 评论分页
         try:
-            comment_page = request.GET.get('page', 1)
+            comment_page = request.GET.get('comment_page', 1)
         except PageNotAnInteger:
             comment_page = 1
         # 每页显示5条记录
-        p2 = Paginator(exp_comment_objects, 5, request=request)
+        p2 = Paginator(exp_comment_objects, 3, request=request)
         comments = p2.page(comment_page)
 
         # 得到答题数目最多的前5个用户,格式:['user_id', '答题数目']
@@ -79,14 +106,24 @@ class ExpView(View):
                     id__in=UserLearn.objects.filter(user_id=user_tunple[0], learn_type=2)).values_list(
                     'category').annotate(count=Count('category')).values_list('count', 'category').order_by(
                     '-count')[0][1]
-            user_entity.category = CATEGORY_CHOICES[category2]  # 筛选出ctf的评论并且按时间倒序排列
+            user_entity.category = CATEGORY_CHOICES[category2]  # 筛选出exp的评论并且按时间倒序排列
 
             # 将用户传入列表
             user_list.append(user_entity)
 
-        tags={"xss",}
+        tags=list(set(tags))
+
+        try:
+            exp_page = request.GET.get('exp_page', 1)
+        except PageNotAnInteger:
+            exp_page = 1
+        # 每页显示4条记录
+        p1 = Paginator(all_exps, 4, request=request)
+        hot_exps = p1.page(exp_page)
+
 
         return render(request, 'exp_list.html', {
+            "category":category,
             "heroes": user_list,
             "all_comment":comments,
             "tags":tags,
