239f459a59e0d286c79765eb81ca1161db3ca972
gatsby <2931799821@qq.com>
2017-07-25 20:35:35 +0800

    修改了发送新消息布局

--- ADP/settings.py
+++ ADP/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "adp",
         'USER': "root",
-        'PASSWORD': "",
+        'PASSWORD': "123456",
         'HOST': "127.0.0.1",
     }
 }
